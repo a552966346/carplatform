36c1f93b5e69515335b1bc16b342e36836da63d2
SPC <552966346@example.com>
2020-09-24 19:26:53 +0800

    vuex,axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import store from './store'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
@@ -10,6 +15,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
