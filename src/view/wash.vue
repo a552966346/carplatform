16efc59c25f64fdc3c708f1a6429799b0e350184
sxb9736 <sxbqq@outlook.com>
2020-10-22 17:23:50 +0800

    洗车页面标点

--- src/view/wash.vue
+++ src/view/wash.vue
@@ -47,6 +47,7 @@ import Swiper from 'swiper';
       return {
         urls: require('../../static/img/daohang.png'),
         urlp: require('../../static/img/phone.png'),
+        urlm: require('../../static/img/wash_weizhi.png'),
         data:[],
         merchant:[],
         category:[],
@@ -57,17 +58,13 @@ import Swiper from 'swiper';
       }
     },
     mounted:function(){
-      var that = this
-      // for(let i=0; i<=that.merchant.length; i++){
-      //    markerss.id =  that.merchant.id;
-      //    markerss.position = new TMap.latLng(that.merchant.lat,that.merchant.lng)
-      // }
+      let that = this
+      let markerss
        that.$store.state.heard_title ='车平台 - 洗车'
-
         //定义地图中心点坐标
-        var center = new TMap.LatLng(39.984120, 116.307484)
+        let center = new TMap.LatLng(39.984120, 116.307484)
         //定义map变量，调用 TMap.Map() 构造函数创建地图
-        var map = new TMap.Map(document.getElementById('container'), {
+        let map = new TMap.Map(document.getElementById('container'), {
             center: center,//设置地图中心点坐标
             zoom: 11,   //设置地图缩放级别
             viewMode:'2D',
@@ -82,31 +79,23 @@ import Swiper from 'swiper';
                 that.detailimages = that.data.detailimages
                 that.$nextTick(function(){
                   that.doswiper()
+                  // console.log(that.marker(that.merchant))
                 })
+                for(let i= 0 ; i<that.merchant.length; i++){
+                  console.log(that.marker(that.merchant[i]))
+                }
+                
             })
-            var marker = new TMap.MultiMarker({
-                        id: 'marker-layer',
-                        map: map,
-                        styles: {
-                            "marker": new TMap.MarkerStyle({
-                                "width": 25,
-                                "height": 35,
-                                "anchor": { x: 16, y: 32 },
-                                "src": 'https://mapapi.qq.com/web/lbs/javascriptGL/demo/img/markerDefault.png'
-                            })
-                        },
-                        // geometries: [markerss]
-                        })
-                    marker.on("click", (evt)=>{
-                      this.shop()
-                    })
+            // marker.on("click", (evt)=>{
+            //   this.shop()
+            // })
     },
     methods: {
       shop() {
-        var that = this;
-        console.log(that.urls)
+        let that = this;
+        //console.log(that.urls)
         layui.use('layer', function() {
-          var layer = layui.layer;
+          let layer = layui.layer;
          // console.log(typeof(that))
         // $(".layui-layer").css("background", "none");
           layer.open({
@@ -120,47 +109,85 @@ import Swiper from 'swiper';
             shade: 0.01,
             id: 'one',
             shadeClose: true, //开启遮罩关闭
-            content: ` <div class="shop" ref="total" v-for="(item,index) in merchant">
-                <div class="upper">
-                  <div class="upper_img">
-                    <img :src="item.banner" />
-                  </div>
-                  <div class="upper_text">
-                    <div class="upper_text_top">
-                      <p>{{item.name}}</p>
-                      <a href="#"><img src="${that.urls}">
-                      导航</a>
-                    </div>
-                    <div class="upper_text_score">
-                      <p>快修店</p>
-                      <p>{{item.star}}级</p>
-                      <p><span>评分</span>{{item.star}}.0</p>
-                    </div>
-                    <div class="upper_text_phont">
-                      <img src="${that.urlp}" >
-                      <p>{{item.mobile}}</p>
-                    </div>
-                  </div>
+            content: `<div class="shop" ref="total" v-for="(item,index) in merchant">
+		    <div class="upper">
+		      <div class="upper_img">
+		        <img :src="item.banner" />
+		      </div>
+		      <div class="upper_text">
+		        <div class="upper_text_top">
+		          <p>{{item.name}}</p>
+		          <a href="#"><img src="${that.urls}">
+		          导航</a>
+		        </div>
+		        <div class="upper_text_score">
+		          <p>快修店</p>
+		          <p>{{item.star}}级</p>
+		          <p><span>评分</span>{{item.star}}.0</p>
+		        </div>
+		        <div class="upper_text_phont">
+		          <img src="${that.urlp}" >
+		          <p>{{item.mobile}}</p>
+		        </div>
+		      </div>
 
-                </div>
-                <div class="bottom">
-                  <div class="bottom_left">
-                    <p>{{item.address}}</p>
-                  </div>
-                  <div class="bottom_right">
-                    <a href="#">立即预约</a>
-                  </div>
-                </div>
-              </div>`
+		    </div>
+        <div class="center">
+          <div class="center_left"><p>精选服务</p></div>
+          <div class="center_right"><img src="../../static/img/wash__layer_one.png" alt=""><p>普洗</p></div>
+          <div class="center_right"><img src="../../static/img/wash__layer_one.png" alt=""><p>精洗</p></div>
+          <div class="center_right"><img src="../../static/img/wash__layer_one.png" alt=""><p>全车内饰清洗</p></div>
+          <div class="center_right"><img src="../../static/img/wash__layer_one.png" alt=""><p>打蜡</p></div>
+        </div>
+		    <div class="bottom">
+		      <div class="bottom_left">
+		        <p>{{item.address}}</p>
+		      </div>
+		      <div class="bottom_right">
+		        <a href="#">立即预约</a>
+		      </div>
+		    </div>
+		  </div> `
           });
         });
       },
-      marker(){
-        var that = this
+      marker(ent){
+        let that = this
+        let markerss = []
+        let id = []
+        let lat = [],lng = []
+       //console.log(ent[0].lat)
+          //console.log(ent[i].lat)
+         lat.push(ent.lat)
+         lng.push(ent.lng)
+         id.push(ent.id)
+         let position =  new TMap.LatLng(lat, lng)
+         let marker = new TMap.MultiMarker({
+                      id: 'container',
+                      //map: map,
+                      styles: {
+                          "marker": new TMap.MarkerStyle({
+                              "width": 25,
+                              "height": 35,
+                              "anchor": { x: 16, y: 32 },
+                              "src": '../../static/img/wash_weizhi.png'
+                          })
+                      },
+                        geometries: [{
+                          "id": id,
+                          "styleId": 'marker',
+                          "position":position,
+                          "properties": {
+                              "title":ent.name
+                          }
+                      }]
+
+                      })
+                      
 
       },
       doswiper(){
-        var mySwiper = new Swiper ('.swiper-container', {
+        let mySwiper = new Swiper ('.swiper-container', {
              loop: true, // 循环模式选项
              slidesPerView: 1.2,
              spaceBetween: -15,
@@ -172,7 +199,7 @@ import Swiper from 'swiper';
                  hideOnClick: true,
                 },
         	})
-      }
+      },
     }
   }
 </script>
